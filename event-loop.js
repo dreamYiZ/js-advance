f4fa80bc3f58c22b88bc24015a70d41427bd8862
Rob <dreamYiZ@outlook.com>
2021-01-12 04:24:12 +0800

    +

---
+++ event-loop.js
@@ -0,0 +1,22 @@
+// function foo (){
+// 	return foo()
+// }
+// foo()
+
+
+
+
+
+// console.log('hi')
+
+// setTimeout( function(){
+// 	console.log('there')
+// }, 0)
+
+// console.log('JSConfEU')
+
+
+
+
+
+
